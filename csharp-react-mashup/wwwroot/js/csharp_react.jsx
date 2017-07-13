03bd75304f511188f418adb61dccd9c6fb3a609b
Logan Kelly <logan.r.kelly@gmail.com>
2017-07-13 17:21:23 -0500

    added comments, extracted data into global variable

--- csharp-react-mashup/wwwroot/js/csharp_react.jsx
+++ csharp-react-mashup/wwwroot/js/csharp_react.jsx
@@ -1,8 +1,24 @@
-﻿var CommentList = React.createClass({
+﻿var data = [
+    { id: 1, author: "Ben Bizzey", text: "Heyyyyyy what it do pal?"},
+    { id: 2, author: "Diggy Wiggler", text: "Hey ya chowdahead, get off my properties!"},
+    { id: 3, author: "Winkle Burgleton", text: "Heyyyyyy who ya callin chowdahead ya codfish"},
+];
+
+
+var CommentList = React.createClass({
     render: function() {
+        //mapping the data array into a generic comment - grabbed from the rendering of the CommentList in the CommentBox component return
+        var commentNodes = this.props.data.map(function(comment) {
+	        return (
+	            <Comment author={comment.author} key={comment.id}>
+	                {comment.text}
+	            </Comment>
+	        );
+        });
+
         return (
             <div className="commentList">
-                Comment List!
+                {commentNodes}
             </div>
         );
     }
@@ -18,19 +34,42 @@ var CommentForm = React.createClass({
     }
 });
 
+//grabs the data being passed in the data property of the ReactDOM render of the CommentBox
 var CommentBox = React.createClass({
     render: function() {
         return (
             <div className="commentBox">
                 <h1>Turtles</h1>
-                <CommentList />
+                <CommentList data={this.props.data} />
                 <CommentForm />
             </div>
         );
     }
 });
 
+var Comment = React.createClass({
+    rawMarkup: function() {
+        var md = new Remarkable();
+        var rawMarkup = md.render(this.props.children.toString());
+        return { __html: rawMarkup };
+    },
+
+    render: function() {
+        return (
+            <div className="comment">
+                <h2 className="commentAuthor">
+                    {this.props.author}
+                </h2>
+                <span dangerouslySetInnerHTML={this.rawMarkup()} />
+            </div>
+        );
+    }
+});
+
 ReactDOM.render(
-    <CommentBox />,
+    //grabs the data from the global variable (to be transferred to database)
+    <CommentBox data={data}/>,
     document.getElementById('content')
 );
+
+
