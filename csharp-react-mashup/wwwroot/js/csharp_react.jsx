caf735931cae039a9ecbfaa9ab3da2008ba85983
Logan Kelly <logan.r.kelly@gmail.com>
2017-07-12 23:51:21 -0500

    removed view home folder in finder to replace with react view home folder

--- csharp-react-mashup/wwwroot/js/csharp_react.jsx
+++ csharp-react-mashup/wwwroot/js/csharp_react.jsx
@@ -1,1 +1,14 @@
-﻿
+﻿var CommentBox = React.createClass({
+    render: function() {
+        return (
+            <div className="commentBox">
+                Hello World, I am Confusing!
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <CommentBox />,
+    document.getElementById('content')
+);
